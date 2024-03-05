710cb504014d0f4c344d66f96200f9b479822d55
YanisRichard <yanis.richard17@gmail.com>
2024-03-05 14:00:05 +0100

    test affichage des svg

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,15 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
+import SvgProfil from '@/components/SvgProfil.vue';
+import SvgDessus from '@/components/SvgDessus.vue';
+
 </script>
 
 <template>
   <main>bonjour de home</main>
+
+  <SvgProfil />
+  <SvgDessus />
+
+  
 </template>
